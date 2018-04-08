bb91e7d9f46e8ed7190a0377e73703335d4b7558
Oleg Silvestrov <silvedko@gmail.com>
2018-04-09 00:32:05 +0300

    Add project

---
+++ Assets/Scripts/PathManager/PathManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathManager : MonoBehaviour
+{
+    Vector3 _lastPoint;
+
+    public Vector3 GetNextPoint()
+    {
+        return Vector3.zero;
+    }
+
+    private void Start()
+    {
+        WeatherController.OnWeatherChanged += OnWeatherChanged;
+
+    }
+
+    private void OnWeatherChanged (Weather obj)
+    {
+        switch (obj)
+        {
+            case Weather.Rain:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private RoadTile GenerateNextTile ()
+    {
+        //var randWidth = Random.Range();
+
+        RoadTile tmpTile = new RoadTile();
+        return tmpTile;
+    }
+
+
+    void Update()
+    {
+
+    }
+}
+
+public struct RoadTile
+{
+    float Width;
+    float Length;
+    bool IsLeftTurn;
+
+    public RoadTile (float width, float length, bool isLeft)
+    {
+        Width = width;
+        Length = length;
+        IsLeftTurn = isLeft;
+    }
+}
+
---
+++ Assets/Scripts/WeatherController/WeatherController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class WeatherController : MonoBehaviour
+{
+    public static event Action<Weather> OnWeatherChanged;
+
+    public Weather CurrentWeather
+    {
+        get { return _currentWEather; }
+        private set
+        {
+            if (_currentWEather != value)
+            {                
+                _currentWEather = value;
+                OnWeatherChanged?.Invoke(value);
+            }
+        }
+    }
+    private Weather _currentWEather;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
+
+public enum Weather
+{
+    Sunny,
+    Rain,
+    Snowy
+}
